2e4c44f4b45e8135d2e2deef8ae6d3d2dfe54bec
Gideon Oba <deyonoba@gmail.com>
2024-07-04 20:59:36 +0100

    update 101-safely_get_value.py file

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
 """
 Script creates a custome type variable
+Concepts:
+    - Mapping.
+    - TypeVar
 """
 from typing import TypeVar, Any, Mapping, Union
 T = TypeVar('T')
 
 
 def safely_get_value(
-    dct: Mapping,
+    dct: Mapping[Any, Any],
     key: Any,
     default: Union[T, None] = None
 ) -> Union[T, None]:

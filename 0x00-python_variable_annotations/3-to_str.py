ae3e46aa3887413ef29f8de963315a1cdc121774
Gideon Oba <deyonoba@gmail.com>
2024-07-04 10:32:04 +0100

    Add 3-to_str.py file

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Script contains a function `to_str`.
+    Function takes a float `n` as argument,
+    and then returns a string representation of the float number.
+"""
+
+
+def to_str(n: float) -> str:
+    """Converts a float number to a string representation."""
+    return str(n)

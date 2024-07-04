f399af8236ef807472f896229f19e555a15f6260
Gideon Oba <deyonoba@gmail.com>
2024-07-04 09:43:10 +0100

    Add 1-concat.py file

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Script contains a type-annotated function `concat`.
+    Function takes two arguments:
+        1. `str1` of string type.
+        2. `str2` of string type.
+    Function returns a concatenated string.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concantenates two strings together."""
+    return str1 + str2

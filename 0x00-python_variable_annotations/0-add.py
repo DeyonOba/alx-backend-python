d6cdc009421513b32e701825605985afba93c18a
Gideon Oba <deyonoba@gmail.com>
2024-07-04 09:33:02 +0100

    Add 0-add.py file

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Write a type-annotated function `add`.
+    Function takes 2 agruments:
+        1. `a` of float type.
+        2. `b` of float type.
+    Function returns the sum of both numbers, return type float
+"""
+
+
+def add(a: float, b: float) -> float:
+    """Sums two float numbers together."""
+    return a + b

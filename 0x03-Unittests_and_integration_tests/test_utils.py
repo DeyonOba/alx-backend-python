3a0d648b3edda075bf2f15cee47d4d1a77e47f1c
Gideon Oba <deyonoba@gmail.com>
2024-07-29 10:48:59 +0100

    Add test_utils.py file

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Unit test and Integation tests.
+"""
+import unittest
+from parameterized import parameterized
+from utils import access_nested_map
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """Unit test for `utils.access_nested_map`"""
+
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+    def test_acces_nested_map(self, nested_map, path, output):
+        self.assertEqual(
+            access_nested_map(nested_map, path),
+            output
+        )
+
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)

e04ffe735ea780eb7795309c27f5b32c887b2b12
Gideon Oba <deyonoba@gmail.com>
2024-07-29 11:03:21 +0100

    modified: test_access_nested_map

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -15,7 +15,7 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
-    def test_acces_nested_map(self, nested_map, path, output):
+    def test_access_nested_map(self, nested_map, path, output):
         """Test the utils method `access_nested_map`."""
         self.assertEqual(access_nested_map(nested_map, path), output)
 

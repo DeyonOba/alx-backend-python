ca022faee784b8aec199bd6eaa5dddcce9abdf4f
Gideon Oba <deyonoba@gmail.com>
2025-12-18 22:15:57 +0100

    Add solution for task 3

---
+++ python-decorators-0x01/3-retry_on_failure.py
@@ -0,0 +1,47 @@
+import sqlite3 
+import functools
+import time
+
+DB_NAME = "users.db"
+query_cache = {}
+
+
+def with_db_connection(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        conn = sqlite3.connect(DB_NAME)
+        try:
+            result = func(conn, *args, **kwargs)
+        finally:
+            conn.close()
+        return result
+    return wrapper
+
+
+def retry_on_failure(retries=3, delay=2):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            last_exception = None
+            for attempt in range(retries):
+                try:
+                    return func(*args, **kwargs)
+                except Exception as e:
+                    last_exception = e
+                    print(f"Attempt {attempt + 1} failed: {e}. Retrying in {delay} seconds...")
+                    time.sleep(delay)
+            raise last_exception
+        return wrapper
+    return decorator
+
+
+@with_db_connection
+@retry_on_failure(retries=3, delay=1)
+def fetch_users_with_retry(conn):
+    cursor: sqlite3.Cursor = conn.cursor()
+    cursor.execute("SELECT * FROM users")
+    return cursor.fetchall()
+
+#### attempt to fetch users with automatic retry on failure
+users = fetch_users_with_retry()
+print(users)
